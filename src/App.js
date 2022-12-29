e66d17bfb948bf25fb5e4f7077de0d106e58e2dd
daniel <daniellisboa2002@gmail.com>
2022-12-29 20:13:16 -0300

    challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState[{}];
+
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+
+    console.log(newDot);
+    setList((prev) => [...prev, newDot]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page" onClick={handleClick}>
+      {JSON.strigfy(list)}
+      <span className="dot" />
     </div>
   );
 }
